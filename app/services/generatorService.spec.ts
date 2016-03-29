5e21543acf10a67235b952063580083b7d78f8ad
Shorty Coder <shortycoder@gmail.com>
2016-03-29 19:30:56 +0200

    implement shortcode generator that uses [A-Z_]

--- app/services/generatorService.spec.ts
+++ app/services/generatorService.spec.ts
@@ -5,9 +5,15 @@ import sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 let expect = chai.expect;
 
-describe('The shortcode generator', ()=>{
-   it('generates a shortcode of the correct length and characters', ()=>{
-       let shortcodeGenerator = new ShortcodeGenerator();
-       expect(shortcodeGenerator.generate()).to.match(/^[0-9a-zA-Z_]{6}$/);
-   });
+describe('The shortcode generator', ()=> {
+    it('generates a shortcode of the correct length and characters', ()=> {
+        let shortcodeGenerator = new ShortcodeGenerator();
+        expect(shortcodeGenerator.generate()).to.match(/^[0-9a-zA-Z_]{6}$/);
+    });
+
+    it('generates a different shortcode on every call', ()=> {
+        let shortcodeGenerator = new ShortcodeGenerator();
+
+        expect(shortcodeGenerator.generate()).not.to.equal(shortcodeGenerator.generate());
+    });
 });
--- app/services/generatorService.ts
+++ app/services/generatorService.ts
@@ -1,11 +1,15 @@
 export class ShortcodeGenerator {
     private length: number;
+    private chars: string;
 
     constructor() {
         this.length = 6;
+        this.chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
     }
-    
+
     public generate(): string {
-        return Math.round((Math.pow(36, this.length + 1) - Math.random() * Math.pow(36, this.length))).toString(36).slice(1);
+        var result = '';
+        for (var i = this.length; i > 0; --i) result += this.chars[Math.floor(Math.random() * this.chars.length)];
+        return result;
     }
 }
